e3ec7e56dcd3c92691e1d1e73a60e05009389a41
so-eli <soelianam@gmail.com>
2021-02-11 19:13:39 -0300

    implementando sass

--- js/scripts.js
+++ js/scripts.js
@@ -10,7 +10,7 @@ $('#navbarResponsive li a').click(function(e){
   var position = $(ancla).position();
   if (window.matchMedia("(min-width: 568px)").matches) {
     /* La pantalla tiene al menos 568 píxeles de ancho */
-    $('html, body').scrollTop( position.top - 76 )
+    $('html, body').scrollTop( position.top - 200 )
     /* $('html, body').animate({scrollTop: (position.top - 76)}); */
   } else {
     /* La pantalla tiene menos de 568 píxeles de ancho */
@@ -18,4 +18,29 @@ $('#navbarResponsive li a').click(function(e){
     /* $('html, body').animate({scrollTop: (position.top - 230)}); */
   }
   
+})
+$('#navbarResponsive li a').click(function(e){
+  e.preventDefault();
+  let ancla = $(this).attr('href');
+  console.log(ancla)
+  var position = $(ancla).position();
+  if (window.matchMedia("(min-width: 992px)").matches) {
+    /* La pantalla tiene al menos 568 píxeles de ancho */
+    $('html, body').scrollTop( position.top - 60 )
+    /* $('html, body').animate({scrollTop: (position.top - 76)}); */
+  } else {
+    /* La pantalla tiene menos de 568 píxeles de ancho */
+    $('html, body').scrollTop( position.top - 200 )
+    /* $('html, body').animate({scrollTop: (position.top - 230)}); */
+  }
+  
+})
+
+
+
+const btnSwitch = document.querySelector('#switch');
+
+btnSwitch.addEventListener('click', () => {
+    document.body.classList.toggle('dark');
+    btnSwitch.classList.toggle('active');
 })
